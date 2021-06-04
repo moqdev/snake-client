986cbd8ccb7f41c5bf2f8ab9f860ed74ffd7bc5d
Mona Waseem <monaqwaseem@gmail.com>
2021-06-04 02:23:54 +0000

    Add setup interface to handle user input from stdin.

--- play.js
+++ play.js
@@ -1,6 +1,16 @@
 // establishes a connection with the game server
 const net = require('net');
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+
 const connect = function () {
   const conn = net.createConnection({
     host: "192.168.0.101",
