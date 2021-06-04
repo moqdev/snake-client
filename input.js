379f31c7846d2fdb730a12927629d2d3ca843a25
Mona Waseem <monaqwaseem@gmail.com>
2021-06-04 03:14:24 +0000

    Add logic to input.js.

--- input.js
+++ input.js
@@ -1,15 +1,36 @@
-const setupInput = function (result) {
-  strt = result;
+const connect = require("./client");
+// Stores the active TCP connection object.
+//let connection;	
+const setupInput = function (conn) {
+  results = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on('data', handleUserInput);
-
   return stdin;
 };
+
 const handleUserInput = function (input) {
-  // const stdin = process.stdin;
+
   if (input === '\u0003') {
     process.exit();
   };
+  if (input === 'w') {
+    connection.write("Move: up");
+  };
+
+  if (input === 'a') {
+    connection.write("Move: left");
+  };
+
+  if (input === 's') {
+    connection.write("Move: down");
+  };
+  if (input === 'd') {
+    connection.write("Move: right");
+  };
+
+};
+
+module.exports = setupInput;
