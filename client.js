68759cff3673cdfe74446d20dc12d580f27e0b92
Mona Waseem <monaqwaseem@gmail.com>
2021-06-04 03:23:19 +0000

    Modify client.js and connect fuction to require and use two constants.

--- client.js
+++ client.js
@@ -1,29 +1,24 @@
 // ES6 shorthand method
 // establishes a connection with the game server
-const { Module } = require('module');
-const net = require('net');
+const { IP, PORT } = require('./constants');
 
+const net = require("net");
+const stdin = process.stdin;
+stdin.setEncoding('utf8');
 const connect = function () {
   const conn = net.createConnection({
     host: IP,
-    port: HOST,
+    port: PORT,// Localhost
   });
 
   // interpret incoming data as text
-  //
   conn.setEncoding("utf8");
-  //initials SSS
-
   conn.on('data', (data) => {
     console.log(data);
   });
-  //conn.write("Move: down");
-  //console.log("down")
-
-
+  conn.write("Name: Teddy");
 
   return conn;
 
 };
-
 module.exports = connect;
