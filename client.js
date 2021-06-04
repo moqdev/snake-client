5d81f37868adf58a8345c5b2544fcfa02601a5b5
Mona Waseem <monaqwaseem@gmail.com>
2021-06-04 02:09:19 +0000

    Register event handler and establish connect event.

---
+++ client.js
@@ -0,0 +1,27 @@
+// ES6 shorthand method
+// establishes a connection with the game server
+const { Module } = require('module');
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP,
+    port: HOST,
+  });
+
+  // interpret incoming data as text
+  //
+  conn.setEncoding("utf8");
+  //initials SSS
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+
+
+  return conn;
+
+};
+
+module.exports = connect;
